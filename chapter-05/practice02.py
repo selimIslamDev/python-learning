6eb93db88518e665e68d652d965bfae17b0c9ee4
Salim <salimislam0036@gmail.com>
2026-05-02 21:01:18 +0600

    Add initial Python scripts for set basics and unique language counting

---
+++ chapter-05/practice02.py
@@ -0,0 +1,3 @@
+# You are given a list of programming languages:
+# ["Python", "Java", "C++", "Python", "Java", "C"]
+# Convert it into a set and print how many unique languages Divya knows.
---
+++ chapter-05/setBasics.py
@@ -0,0 +1,11 @@
+food = {"food1", "food2", "food3"}
+print(food)
+food.add("food4")
+print(food)
+print(type(food))
+
+
+#set do not entertain duplicate
+
+emptySet = set()
+print(emptySet)
