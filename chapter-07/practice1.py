0b4b585863afb985c5a42fa516db39b1c9995587
Salim <salimislam0036@gmail.com>
2026-05-17 21:38:58 +0600

    Add welcome_message function to print greeting three times

---
+++ chapter-07/practice1.py
@@ -0,0 +1,6 @@
+# Write a function named welcome_message() that prints "Welcome to Python Programming!" three times.
+def welcome_message():
+    for i in range(3):
+        print("Welcome to Python Programming!")
+
+welcome_message()
