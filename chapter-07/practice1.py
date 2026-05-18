f5b85f3c6758247c10da82752b6f01b490426414
Salim <salimislam0036@gmail.com>
2026-05-18 22:37:29 +0600

    Remove instructional comment from practice1.py

--- chapter-07/practice1.py
+++ chapter-07/practice1.py
@@ -1,4 +1,4 @@
-# Write a function named welcome_message() that prints "Welcome to Python Programming!" three times.
+
 def welcome_message():
     for i in range(3):
         print("Welcome to Python Programming!")
