bfad85b27767b44b92508322735f409153384008
Salim <salimislam0036@gmail.com>
2026-05-19 22:44:37 +0600

    Add functionParam.py with average function definition and example calls

---
+++ chapter-07/functionParam.py
@@ -0,0 +1,9 @@
+#function defination with parameters
+def average(a,b):
+    averageValue = (a+b)/2
+    print("Average of",a,"and",b,"is",averageValue)
+
+
+average(10,20)
+average(15,25)
+average(100,200)
